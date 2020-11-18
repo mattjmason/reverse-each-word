0007ee3cc56bf33b5442089f9d78ed58c53c0bac
Matt Mason <masonmj10@gmail.com>
2020-11-17 21:26:35 -0500

    Update reverse_each_word.rb

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,4 @@
-
-
-
-
-
 def reverse_each_word(phrase)
-    phrase.split(" ").collect {|word| word.reverse!}.join(" ")
-  end
-
-
-  def reverse_each_word(phrase)
     new_phrase = phrase.split(" ")
     new_phrase.collect do |word| word.reverse!
     end
